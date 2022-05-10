da7658276d68047fd69a5fee2e75c41792f1428f
jtenerelli017 <41691080+jtenerelli017@users.noreply.github.com>
2022-05-10 14:56:56 -0400

    Removed redundant libraries

--- SWARM/Server/Controllers/Course/CourseController.cs
+++ SWARM/Server/Controllers/Course/CourseController.cs
@@ -1,19 +1,11 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SWARM.EF.Data;
 using SWARM.EF.Models;
-using SWARM.Server.Models;
-using SWARM.Shared;
-using SWARM.Shared.DTO;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Telerik.DataSource;
 using Telerik.DataSource.Extensions;
 
 namespace SWARM.Server.Controllers.Crse
